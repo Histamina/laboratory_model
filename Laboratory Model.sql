129130f9412b131336861402f6f936277606f860
Julieta Gomez <55216740+Histamina@users.noreply.github.com>
2024-07-31 22:40:30 -0300

    Add FKs to branch offices table

--- Laboratory Model.sql
+++ Laboratory Model.sql
@@ -8,7 +8,7 @@ USE laboratory_model;
 # CREATE TABLES
 
 CREATE TABLE PATIENTS(
-    patient_id INT NOT NULL AUTO_INCREMENT,
+	patient_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     document_type VARCHAR(30) NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE PATIENTS(
 );
 
 CREATE TABLE TECHNICIANS(
-    technician_id INT NOT NULL AUTO_INCREMENT,
+	technician_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     document_type VARCHAR(30) NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE TECHNICIANS(
 );
 
 CREATE TABLE ASSISTANCE_RECORDS(
-    assistance_id INT NOT NULL AUTO_INCREMENT,
+	assistance_id INT NOT NULL AUTO_INCREMENT,
     date_time DATETIME NOT NULL,
     patient_id INT,
     technician_id INT,
@@ -38,7 +38,7 @@ CREATE TABLE ASSISTANCE_RECORDS(
 );
 
 CREATE TABLE BIOCHEMISTS(
-    biochemist_id INT NOT NULL AUTO_INCREMENT,
+	biochemist_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     document_type VARCHAR(30) NOT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE BIOCHEMISTS(
 );
 
 CREATE TABLE EXAMS(
-    exam_id INT NOT NULL AUTO_INCREMENT,
+	exam_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(60) NOT NULL,
     result VARCHAR(70),
     biochemist_id INT,
@@ -61,21 +61,22 @@ CREATE TABLE EXAMS(
 );
 
 CREATE TABLE BRANCH_OFFICES(
-    branch_office_id INT NOT NULL AUTO_INCREMENT,
+	branch_office_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     address VARCHAR(40) NOT NULL,
     phone VARCHAR(40) NOT NULL,
     technician_id INT,
     biochemist_id INT,
-    PRIMARY KEY(branch_office_id)
+    PRIMARY KEY(branch_office_id),
+    FOREIGN KEY(technician_id) REFERENCES TECHNICIANS(technician_id),
+    FOREIGN KEY(biochemist_id) REFERENCES BIOCHEMISTS(biochemist_id)
 );
 
 CREATE TABLE PATIENT_VISITS(
-    visit_id INT NOT NULL AUTO_INCREMENT,
+	visit_id INT NOT NULL AUTO_INCREMENT,
     patient_id INT,
     branch_office_id INT,
     PRIMARY KEY(visit_id),
     FOREIGN KEY(patient_id) REFERENCES PATIENTS(patient_id),
     FOREIGN KEY(branch_office_id) REFERENCES BRANCH_OFFICES(branch_office_id)
 );
-
